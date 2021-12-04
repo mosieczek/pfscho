685ac6b1a4e82962a3a253c95ed7653bde0a5c7d
mosieczek <adaosmulska@gmail.com>
2021-12-04 17:56:22 +0100

    lab8

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM node:alpine
-ENV NODE_OPTIONS=--openssl-legacy-provider
+
 WORKDIR '/app'
+ENV NODE_OPTIONS=--openssl-legacy-provider
 COPY package.json .
 RUN npm install
-COPY . .
-RUN npm run build
 
-FROM nginx
-EXPOSE 80
-COPY --from=0 /app/build /usr/share/nginx/html
+RUN chown -R node:node /app
+
+CMD ["npm", "run", "start"]
--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
 
 function App() {
+  const [value, setValue] = useState(0)
+  const [result, setResult] = useState(0)
+  function count(){
+    setResult(0)
+    if(parseInt(value) === 1 || parseInt(value) === 2){
+      setResult(1)
+    }
+    else{
+      let firstVal = 1, secondVal = 1, tempRes 
+      for(let i = 2; i < value; i++){
+        tempRes = firstVal + secondVal
+        firstVal = secondVal
+        secondVal = tempRes
+        setResult(tempRes)
+      }
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 123 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label for='inputValue'>Podaj liczbe naturalna K:</label>
+        <input id='inputValue' onChange={(e) => {setValue(e.target.value)}} type='text'/>
+        <br/>
+        <button onClick={count}>Oblicz</button>
+        K-ty element ciagu Fibonacciego: {result}
       </header>
     </div>
   );
